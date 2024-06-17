cd03714fab57ed2ba80fa9730bcd3df42fd33e04
shubham paul <91714143+Shu12388y@users.noreply.github.com>
2024-06-17 21:44:58 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+# get
+# api/v1/getproduct
+ # api/v1/postProduct
+
+# {
+#     "name":"product name",
+#     imageUrl:"",
+#     decription:"",
+#     livepreview:""
+# }
+# mongodb
+# ODM  ---> mongodb
+# cors --> 
+# helmet
+# health /api/v1/health status(200).json()
